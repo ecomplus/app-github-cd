97e6d53b6c0fc360370316c300652580bee7f0e4
Leonardo Matos <leomp120894@gmail.com>
2019-04-01 16:09:57 -0300

    feat: setup endpoints for github oauth and webhooks (nothing to do)

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,8 +1,9 @@
 'use strict'
 
-const GET = () => {
+const GET = (id, meta, body, respond) => {
   // not using GitHub OAuth
   // nothing to do
+  respond(0)
 }
 
 module.exports = {
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const POST = (id, meta, body, respond) => {
+  // not using GitHub webhooks
+  // nothing to do
+  respond(1)
+}
+
+module.exports = {
+  POST
+}
